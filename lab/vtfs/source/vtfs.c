5d34da011dde488057c4fdfaf1a0da3a2e36d771
SteRus <stepa@gmail.com>
2025-01-23 19:29:33 +0300

    task 3 Не до конца понял смысл функции iterate, она заходит в бесконечный цикл

--- lab/vtfs/source/vtfs.c
+++ lab/vtfs/source/vtfs.c
@@ -2,6 +2,7 @@
 #include <linux/module.h>
 #include <linux/printk.h>
 #include <linux/fs.h>
+#include <linux/stat.h>
 
 #define MODULE_NAME "vtfs"
 
@@ -10,6 +11,63 @@ MODULE_AUTHOR("secs-dev");
 MODULE_DESCRIPTION("A simple FS kernel module");
 
 #define LOG(fmt, ...) pr_info("[" MODULE_NAME "]: " fmt, ##__VA_ARGS__)
+
+
+struct dentry* vtfs_lookup(
+  struct inode* parent_inode,  // родительская нода
+  struct dentry* child_dentry, // объект, к которому мы пытаемся получить доступ
+  unsigned int flag            // неиспользуемое значение
+){
+  return NULL;
+}
+
+int vtfs_iterate(struct file* filp, struct dir_context* ctx) {
+  char fsname[10];
+  struct dentry* dentry = filp->f_path.dentry;
+  struct inode* inode   = dentry->d_inode;
+  unsigned long offset  = filp->f_pos;
+  int stored            = 0;
+  ino_t ino             = inode->i_ino;
+
+  unsigned char ftype;
+  ino_t dino;
+  printk("ino = %ld, offset = %ld\n", ino, offset);
+  while (true) {
+    if (ino == 1000) {
+      if (offset == 0) {
+        strcpy(fsname, ".");
+        ftype = DT_DIR;
+        dino = ino;
+        offset++;
+
+      } else if (offset == 1) {
+        strcpy(fsname, "..");
+        ftype = DT_DIR;
+        dino = dentry->d_parent->d_inode->i_ino;
+        offset++;
+
+      } else if (offset == 2) {
+        strcpy(fsname, "test.txt");
+        ftype = DT_REG;
+        dino = 101;
+        offset++;
+
+      } else {
+        return stored;
+      }
+    } else {
+      return -stored;
+    }
+  }
+}
+
+struct inode_operations vtfs_inode_ops = {
+  .lookup = vtfs_lookup,
+};
+
+struct file_operations vtfs_dir_ops = {
+  .iterate = vtfs_iterate,
+};
 
 void vtfs_kill_sb(struct super_block* sb) {
   printk(KERN_INFO "vtfs super block is destroyed. Unmount successfully.\n");
@@ -27,11 +85,13 @@ struct inode* vtfs_get_inode(
   }
 
   inode->i_ino = i_ino;
+  inode->i_op = &vtfs_inode_ops;
+  inode->i_fop = &vtfs_dir_ops;
   return inode;
 }
 
 int vtfs_fill_super(struct super_block *sb, void *data, int silent) {
-  struct inode* inode = vtfs_get_inode(sb, NULL, S_IFDIR, 1000);
+  struct inode* inode = vtfs_get_inode(sb, NULL, S_IFDIR | S_IRWXU | S_IRWXG | S_IRWXO, 1000);
 
   sb->s_root = d_make_root(inode);
   if (sb->s_root == NULL) {
