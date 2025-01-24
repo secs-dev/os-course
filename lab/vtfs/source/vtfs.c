74297fae4d53e9cdaa3f4e5c267b69a9498cf6d4
Local Piper <artsor53@gmail.com>
2025-01-24 17:14:47 +0300

    feat: p3 done

--- lab/vtfs/source/vtfs.c
+++ lab/vtfs/source/vtfs.c
@@ -19,7 +19,62 @@ void vtfs_kill_sb(struct super_block*);
 struct dentry* vtfs_mount(struct file_system_type*, int, const char*, void*);
 int vtfs_fill_super(struct super_block*, void*, int);
 struct inode* vtfs_get_inode(struct super_block*, const struct inode*, umode_t, int);
+struct dentry* vtfs_lookup(struct inode*, struct dentry*, unsigned int);
+int vtfs_iterate(struct file*, struct dir_context*);
 
+struct file_operations vtfs_dir_ops = {
+    .iterate_shared = vtfs_iterate,
+};
+
+int vtfs_iterate(struct file* flip, struct dir_context* ctx) {
+  struct dentry* dentry = flip->f_path.dentry;
+  struct inode* inode = dentry->d_inode;
+  unsigned long offset = ctx->pos;
+  ino_t ino = inode->i_ino;
+
+  // for now we must keep fixed list of entries
+  // standard solution involved iterating inside infinite loop with an upper condition,
+  // which when not satisfied will throw kernel into panic
+  struct {
+    const char* name;
+    unsigned char ftype;
+    ino_t ino;
+  } entries[] = {
+      {       ".", DT_DIR,                              ino},
+      {      "..", DT_DIR, dentry->d_parent->d_inode->i_ino},
+      {"test.txt", DT_REG,                              101},
+  };
+
+  int total_entries = sizeof(entries) / sizeof(entries[0]);
+
+  if (offset >= total_entries) {
+    return 0;
+  }
+
+  for (; offset < total_entries; offset++) {
+    if (!dir_emit(
+            ctx,
+            entries[offset].name,
+            strlen(entries[offset].name),
+            entries[offset].ino,
+            entries[offset].ftype
+        )) {
+      return -ENOMEM;
+    }
+    ctx->pos++;
+  }
+  return 0;
+}
+
+struct inode_operations vtfs_inode_ops = {
+    .lookup = vtfs_lookup,
+};
+
+struct dentry* vtfs_lookup(
+    struct inode* parent_inode, struct dentry* child_dentry, unsigned int flag
+) {
+  return NULL;
+}
 struct inode* vtfs_get_inode(
     struct super_block* sb, const struct inode* dir, umode_t mode, int i_ino
 ) {
@@ -27,13 +82,16 @@ struct inode* vtfs_get_inode(
   struct mnt_idmap* idmap = &nop_mnt_idmap;
   if (inode != NULL) {
     inode_init_owner(idmap, inode, dir, mode);
+    inode->i_mode = mode;
   }
   inode->i_ino = i_ino;
   return inode;
 }
 
 int vtfs_fill_super(struct super_block* sb, void* data, int silent) {
-  struct inode* inode = vtfs_get_inode(sb, NULL, S_IFDIR, 1000);
+  struct inode* inode = vtfs_get_inode(sb, NULL, S_IFDIR | 0777, 100);
+  inode->i_op = &vtfs_inode_ops;
+  inode->i_fop = &vtfs_dir_ops;
   sb->s_root = d_make_root(inode);
   if (sb->s_root == NULL) {
     return -ENOMEM;
