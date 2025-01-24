27bb01061e675b744961a113512fe3f507c0490b
Local Piper <artsor53@gmail.com>
2025-01-24 20:48:37 +0300

    feat: p6 done

--- lab/vtfs/source/vtfs.c
+++ lab/vtfs/source/vtfs.c
@@ -15,7 +15,20 @@ MODULE_DESCRIPTION("A simple FS kernel module");
 
 #define LOG(fmt, ...) pr_info("[" MODULE_NAME "]: " fmt, ##__VA_ARGS__)
 
-static int mask = 0;
+struct vtfs_file {
+  struct list_head list;
+  char* name;
+  ino_t ino;
+  umode_t mode;
+  struct inode* inode;
+  size_t size;
+  char* data;
+};
+
+struct vtfs_dir {
+  struct list_head children;
+  struct vtfs_file* self;
+};
 
 void vtfs_kill_sb(struct super_block*);
 struct dentry* vtfs_mount(struct file_system_type*, int, const char*, void*);
@@ -33,6 +46,7 @@ struct file_operations vtfs_dir_ops = {
 struct inode_operations vtfs_inode_ops = {
     .lookup = vtfs_lookup,
     .create = vtfs_create,
+    .unlink = vtfs_unlink,
 };
 
 int vtfs_create(
@@ -40,91 +54,101 @@ int vtfs_create(
     struct inode* parent_inode,
     struct dentry* child_dentry,
     umode_t mode,
-    bool b
+    bool excl
 ) {
-  ino_t root = parent_inode->i_ino;
-  const char* name = child_dentry->d_name.name;
-  if (root == 100 && !strcmp(name, "test.txt")) {
-    struct inode* inode = vtfs_get_inode(parent_inode->i_sb, NULL, S_IFREG | 0777, 101);
-    inode->i_op = &vtfs_inode_ops;
-    inode->i_fop = NULL;
-
-    d_add(child_dentry, inode);
-    mask |= 1;
-  } else if (root == 100 && !strcmp(name, "new_file.txt")) {
-    struct inode* inode = vtfs_get_inode(parent_inode->i_sb, NULL, S_IFREG | 07777, 102);
-    inode->i_op = &vtfs_inode_ops;
-    inode->i_fop = NULL;
-
-    d_add(child_dentry, inode);
-    mask |= 2;
+  if (S_ISDIR(mode)) {
+    LOG("Directory creation is not supported yet\n");
+    return -EPERM;
   }
+
+  struct vtfs_dir* parent_dir = parent_inode->i_private;
+  struct vtfs_file* new_file;
+
+  struct list_head* pos;
+  list_for_each(pos, &parent_dir->children) {
+    struct vtfs_file* entry = list_entry(pos, struct vtfs_file, list);
+    if (strcmp(entry->name, child_dentry->d_name.name) == 0) {
+      return -EEXIST;
+    }
+  }
+
+  new_file = kmalloc(sizeof(struct vtfs_file), GFP_KERNEL);
+  if (!new_file)
+    return -ENOMEM;
+
+  new_file->name = kstrdup(child_dentry->d_name.name, GFP_KERNEL);
+  new_file->ino = get_next_ino();
+  new_file->mode = mode;
+  new_file->size = 0;
+  new_file->data = NULL;
+
+  new_file->inode = vtfs_get_inode(parent_inode->i_sb, parent_inode, mode, new_file->ino);
+  new_file->inode->i_private = NULL;
+
+  list_add(&new_file->list, &parent_dir->children);
+  d_add(child_dentry, new_file->inode);
   return 0;
 }
 
 int vtfs_unlink(struct inode* parent_inode, struct dentry* child_dentry) {
+  struct vtfs_dir* parent_dir = parent_inode->i_private;
   const char* name = child_dentry->d_name.name;
-  ino_t root = parent_inode->i_ino;
-  if (root == 100 && !strcmp(name, "test.txt")) {
-    mask &= ~1;
-  } else if (root == 100 && !strcmp(name, "new_file.txt")) {
-    mask &= ~2;
+
+  struct list_head *pos, *tmp;
+  list_for_each_safe(pos, tmp, &parent_dir->children) {
+    struct vtfs_file* entry = list_entry(pos, struct vtfs_file, list);
+    if (strcmp(entry->name, name) == 0) {
+      list_del(&entry->list);
+      d_delete(child_dentry);
+      kfree(entry->name);
+      kfree(entry->data);
+      kfree(entry);
+      return 0;
+    }
   }
-  return 0;
+  return -ENOENT;
 }
 
 int vtfs_iterate(struct file* flip, struct dir_context* ctx) {
-  struct dentry* dentry = flip->f_path.dentry;
-  struct inode* inode = dentry->d_inode;
+  struct vtfs_dir* dir = flip->f_inode->i_private;
+  struct list_head* pos;
   unsigned long offset = ctx->pos;
-  ino_t ino = inode->i_ino;
+  unsigned long index = 0;
 
-  // for now we must keep fixed list of entries
-  // standard solution involved iterating inside infinite loop with an upper condition,
-  // which when not satisfied will throw kernel into panic
-  struct {
-    const char* name;
-    unsigned char ftype;
-    ino_t ino;
-  } entries[] = {
-      {       ".", DT_DIR,                              ino},
-      {      "..", DT_DIR, dentry->d_parent->d_inode->i_ino},
-      {"test.txt", DT_REG,                              101},
-  };
+  list_for_each(pos, &dir->children) {
+    if (index++ < offset)
+      continue;
 
-  int total_entries = sizeof(entries) / sizeof(entries[0]);
-
-  if (offset >= total_entries) {
-    return 0;
-  }
-
-  for (; offset < total_entries; offset++) {
+    struct vtfs_file* entry = list_entry(pos, struct vtfs_file, list);
     if (!dir_emit(
             ctx,
-            entries[offset].name,
-            strlen(entries[offset].name),
-            entries[offset].ino,
-            entries[offset].ftype
+            entry->name,
+            strlen(entry->name),
+            entry->ino,
+            S_ISDIR(entry->mode) ? DT_DIR : DT_REG
         )) {
       return -ENOMEM;
     }
     ctx->pos++;
   }
+
   return 0;
 }
 
 struct dentry* vtfs_lookup(
     struct inode* parent_inode, struct dentry* child_dentry, unsigned int flag
 ) {
-  ino_t root = parent_inode->i_ino;
-  const char* name = child_dentry->d_name.name;
-  if (root == 100 && !strcmp(name, "test.txt")) {
-    struct inode* inode = vtfs_get_inode(parent_inode->i_sb, NULL, S_IFREG, 101);
-    d_add(child_dentry, inode);
-  } else if (root == 100 && !strcmp(name, "dir")) {
-    struct inode* inode = vtfs_get_inode(parent_inode->i_sb, NULL, S_IFDIR, 200);
-    d_add(child_dentry, inode);
+  struct vtfs_dir* parent_dir = parent_inode->i_private;
+
+  struct list_head* pos;
+  list_for_each(pos, &parent_dir->children) {
+    struct vtfs_file* entry = list_entry(pos, struct vtfs_file, list);
+    if (strcmp(entry->name, child_dentry->d_name.name) == 0) {
+      d_add(child_dentry, entry->inode);
+      return NULL;
+    }
   }
+
   return NULL;
 }
 struct inode* vtfs_get_inode(
@@ -141,14 +165,44 @@ struct inode* vtfs_get_inode(
 }
 
 int vtfs_fill_super(struct super_block* sb, void* data, int silent) {
-  struct inode* inode = vtfs_get_inode(sb, NULL, S_IFDIR | 0777, 100);
-  inode->i_op = &vtfs_inode_ops;
-  inode->i_fop = &vtfs_dir_ops;
-  sb->s_root = d_make_root(inode);
-  if (sb->s_root == NULL) {
+  struct vtfs_dir* root_dir;
+  struct vtfs_file* root_file;
+
+  root_dir = kmalloc(sizeof(struct vtfs_dir), GFP_KERNEL);
+  if (!root_dir) {
     return -ENOMEM;
   }
-  printk(KERN_INFO "return 0\n");
+
+  INIT_LIST_HEAD(&root_dir->children);
+
+  root_file = kmalloc(sizeof(struct vtfs_file), GFP_KERNEL);
+  if (!root_file) {
+    kfree(root_dir);
+    return -ENOMEM;
+  }
+
+  root_file->name = kstrdup("/", GFP_KERNEL);
+  root_file->ino = 100;
+  root_file->mode = S_IFDIR | 0777;
+  root_file->inode = vtfs_get_inode(sb, NULL, root_file->mode, root_file->ino);
+  root_file->data = NULL;
+  root_file->size = 0;
+
+  root_dir->self = root_file;
+
+  root_file->inode->i_private = root_dir;
+  root_file->inode->i_op = &vtfs_inode_ops;
+  root_file->inode->i_fop = &vtfs_dir_ops;
+
+  sb->s_root = d_make_root(root_file->inode);
+  if (!sb->s_root) {
+    kfree(root_file->name);
+    kfree(root_file);
+    kfree(root_dir);
+    return -ENOMEM;
+  }
+
+  LOG("Superblock initialized");
   return 0;
 }
 
