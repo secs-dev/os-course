9959bfd0ae2ac3b9dbadfbb62757802facdc8b96
vityaman <vityaman.dev@yandex.ru>
2025-08-31 11:20:28 +0300

    Add seq test (#8)
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

---
+++ lab/vtpc/test/test_seq.cpp
@@ -0,0 +1,36 @@
+#include <exception>
+#include <iostream>
+#include <memory>
+
+#include "cmp_file.hpp"
+#include "file.hpp"
+
+auto main() -> int try {
+  constexpr size_t count = 1024;
+
+  auto libc = vt::file::open_libc("/tmp/a");
+  auto vtpc = vt::file::open_vtpc("/tmp/b");
+  vt::cmp_file cmp(std::move(libc), std::move(vtpc));
+
+  cmp.seek(0);
+  for (size_t i = 0; i < count; ++i) {
+    std::string text = std::to_string(i);
+    cmp.write(text);
+  }
+
+  cmp.seek(0);
+  for (size_t i = 0; i < count; ++i) {
+    std::string expected = std::to_string(i);
+    std::string actual = cmp.read(expected.size());
+    if (expected != actual) {
+      throw vt::exception() << "'" << expected << "' != '" << actual << "'";
+    }
+  }
+
+  cmp.sync();
+
+  return 0;
+} catch (const std::exception& e) {
+  std::cerr << "exception: " << e.what() << '\n';
+  return 1;
+}
