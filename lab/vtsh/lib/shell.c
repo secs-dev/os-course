c35533c9e5a47d11c0ba19a8797902b43a360c4a
EkateK1 <hmnxstkbsej@gmail.com>
2025-10-07 12:13:57 +0300

    remove redirection

--- lab/vtsh/lib/shell.c
+++ lab/vtsh/lib/shell.c
@@ -18,31 +18,22 @@ int run(char** buf, size_t* len) {
   char* file_in = 0;
   char* file_out = 0;
   char* del = "\n\t \v\f";
-  // FILE* saved_stdout = stdout;
-  // FILE* saved_stdin = stdin;
 
   char** args = parse_args(*buf, *len, DEL);
 
-  char** res_args = redirection_parsing(args, *len, &file_in, &file_out);
+  // char** res_args = redirection_parsing(args, *len, &file_in, &file_out);
 
-  if (res_args == NULL) {
-    return 0;
-  }
-
-  // printf("%s %s", file_in, file_out);
-
-  // for (size_t i = 0; res_args[i] != NULL; i++) {
-  //   printf("%s 222 ", args[i]);
+  // if (res_args == NULL) {
+  //   return 0;
   // }
-  // printf("\n");
 
   pid_t pid = fork();
   if (pid == 0) {
-    int can_exec = redirection(&file_in, &file_out);
-    if (can_exec == -1) {
-      exit(1);
-    }
-    int res = execvp(res_args[0], res_args);
+    // int can_exec = redirection(&file_in, &file_out);
+    // if (can_exec == -1) {
+    //   exit(1);
+    // }
+    int res = execvp(args[0], args);
     if (res == -1) {
       printf("Command not found\n");
     }
@@ -52,6 +43,6 @@ int run(char** buf, size_t* len) {
     int status;
     waitpid(pid, &status, 0);
   }
-  // free(res_args);
+  free(args);
   return 0;
 }
