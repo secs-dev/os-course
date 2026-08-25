682f2cf56c9fa2b6de8806c514712b5c0e22f0dc
e1turin <ivturnik2003@gmail.com>
2026-08-25 15:22:30 +0300

    update graph_traverse_mmap.c to follow new graph structure

--- lab/intro-exp/src/graph_traverse_mmap.c
+++ lab/intro-exp/src/graph_traverse_mmap.c
@@ -15,7 +15,7 @@
 #  include <endian.h>
 #endif
 
-#define HEADER_SIZE     64
+#define HEADER_SIZE     40
 #define MAGIC           "GCACHEG1"
 #define MAGIC_LEN       8
 
@@ -26,11 +26,7 @@ typedef struct {
     uint64_t node_count;
     uint32_t record_size;
     uint32_t fan_out;
-    uint32_t page_size;
-    uint32_t back_prob_permille;
-    uint64_t seed;
     uint64_t root_index;
-    uint64_t min_step_nodes;
     uint32_t flags;
 } __attribute__((packed)) Header;
 
